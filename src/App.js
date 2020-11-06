fc7a2c20901e26cf5c25ed3308fa9979c03c15e9
franklam2008 <franklam2008@gmail.com>
2020-11-06 15:15:20 -0500

    add axio and log users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React, { useState, useEffect, } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const url = `https://api.github.com/users?since=1`;
+    axios.get(url).then((response) => {
+      setUsers((users) => [...users, ...response.data]);
+    });
+  }, []);
+  console.log(users);
   return (
     <div className="App">
       <header className="App-header">
